0039a15dab93620e726a2ef799e2c3d97e43af1f
UrelMattis <urelmattis@yahoo.com>
2019-02-10 20:02:26 -0500

    Add files via upload

---
+++ catalog/catalogitems.py
@@ -0,0 +1,70 @@
+# imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Category, Base, Item, User
+
+# Bind the engine to the metadata of the Base class
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# Create dummy user
+newUser1 = User(name="John Doe", email="johndoe@johndoe.com",
+                picture='https://cdn2.iconfinder.com/data/icons/website-icons/512/User_Avatar-512.png')
+session.add(newUser1)
+session.commit()
+
+# teams for baskbetball
+sport1 = Category(user_id=1, name="Basketball")
+
+session.add(sport1)
+session.commit()
+
+team1 = Item(user_id=1, name="LA Lakers", description="Team in Los Angeles, CA",
+             level="Professional", category=sport1)
+
+session.add(team1)
+session.commit()
+
+
+team2 = Item(user_id=1, name="Rutgers University",
+             description="Team in New Jersey", level="Collegiate", category=sport1)
+
+session.add(team2)
+session.commit()
+
+team3 = Item(user_id=1, name="Boston Celtics",
+             description="Team in Boston, MA", level="Professional", category=sport1)
+
+session.add(team3)
+session.commit()
+
+# teams for football
+sport2 = Category(user_id=1, name="Football")
+
+session.add(sport2)
+session.commit()
+
+
+team1 = Item(user_id=1, name="University of North Carolina",
+             description="Team in Chapel Hill, NC", level="Collegiate", category=sport2)
+
+session.add(team1)
+session.commit()
+
+team2 = Item(user_id=1, name="Atlanta, Falcons",
+             description="Team inAtlanta, GA", level="Professional", category=sport2)
+
+session.add(team2)
+session.commit()
+
+team3 = Item(user_id=1, name="Arizona Cardinals",
+             description="Team in Arizona", level="Professional", category=sport2)
+
+session.add(team3)
+session.commit()
+
+print "dummy information has been added!"
